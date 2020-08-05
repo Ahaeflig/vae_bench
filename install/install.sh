8b597cd3197739a49a7404d23c8187fcdb69dcb5
ahaeflig <adan.haefliger@gmail.com>
2020-08-06 00:26:26 +0900

    Simplify install script

--- install/install.sh
+++ install/install.sh
@@ -3,14 +3,18 @@
 # Install the docker image
 docker build install/. -t vae:latest
 
+# ================================================
+# Below are commands to update the image if needed
+# ================================================
+
 # Delete the container if a previous version already exists, TODO delete iff it exists
-echo 'Trying to delete container'
-docker container rm vae_container
+# echo 'Trying to delete container'
+# docker container rm vae_container
 
 # Create the container and Install python depedencies
-docker run -dit --name vae_container --gpus all -v $PWD:/home/vae/project/ vae:latest
-docker exec vae_container pip install -r install/requirements.txt
-docker stop vae_container
+# docker run -dit --name vae_container --gpus all -v $PWD:/home/vae/project/ vae:latest
+# docker exec vae_container pip install -r install/requirements.txt
+# docker stop vae_container
 
 # Update the image with the installed depedencies
-docker commit $(docker ps -aqf "name=^vae_container$") vae:latest
+# docker commit $(docker ps -aqf "name=^vae_container$") vae:latest
